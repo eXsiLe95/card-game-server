260aa1e4a722523450cc43568e69294487e1c567
Lukas Kimpel <mail@lukaskimpel.com>
2019-01-16 14:05:59 +0100

    Add apidoc

--- src/games/skipbo.ts
+++ src/games/skipbo.ts
@@ -4,6 +4,20 @@ export class Skipbo {
 
     public static init(app: Express) {
 
+        /**
+         * @api {get} /api/skipbo/new Start a new Skip-bo game session
+         * @apiName GetSkipboNew
+         * @apiGroup Skipbo
+         * @apiVersion 0.0.1
+         *
+         * @apiSuccess {String} message A success message stating a new game has been started
+         *
+         * @apiSuccessExample Success-Response:
+         *  HTTP/1.1 200 OK
+         *  {
+         *      "message": "New game started!"
+         *  }
+         */
         app.get('/api/skipbo/new', (req: Request, res: Response) => {
             res.status(200).send({
                 message: 'New game started!',
