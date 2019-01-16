3898a30314ab31b5137b11454f3b1aacd172682a
Lukas Kimpel <mail@lukaskimpel.com>
2019-01-16 09:08:07 +0100

    Add tests for http headers

--- test/server-test.ts
+++ test/server-test.ts
@@ -1,6 +1,7 @@
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import serverImport = require('../src/server');
+
 const should = chai.should();
 
 const server = serverImport.app;
@@ -17,6 +18,64 @@ describe('Server Test', () => {
                     res.body.message.should.be.an('string');
                     done();
                 });
+        });
+    });
+
+    describe('SECURITY', () => {
+        server._router.stack.forEach((layer) => {
+            if (layer.route && layer.route.path) {
+                describe('route ' + layer.route.path, () => {
+                    it('should set Strict-Transport-Security to max-age=15552000; includeSubDomains', (done) => {
+                        chai.request(server)
+                            .get(layer.route.path)
+                            .end((err, res) => {
+                                res.should.have.header('Strict-Transport-Security',
+                                    'max-age=15552000; includeSubDomains');
+                                done();
+                            });
+                    });
+                    it('should set X-Content-Type-Options to nosniff', (done) => {
+                        chai.request(server)
+                            .get(layer.route.path)
+                            .end((err, res) => {
+                                res.should.have.header('X-Content-Type-Options', 'nosniff');
+                                done();
+                            });
+                    });
+                    it('should set X-DNS-Prefetch-Control to off', (done) => {
+                        chai.request(server)
+                            .get(layer.route.path)
+                            .end((err, res) => {
+                                res.should.have.header('X-DNS-Prefetch-Control', 'off');
+                                done();
+                            });
+                    });
+                    it('should set X-Download-Options to noopen', (done) => {
+                        chai.request(server)
+                            .get(layer.route.path)
+                            .end((err, res) => {
+                                res.should.have.header('X-Download-Options', 'noopen');
+                                done();
+                            });
+                    });
+                    it('should set X-Frame-Options to SAMEORIGIN', (done) => {
+                        chai.request(server)
+                            .get(layer.route.path)
+                            .end((err, res) => {
+                                res.should.have.header('X-Frame-Options', 'SAMEORIGIN');
+                                done();
+                            });
+                    });
+                    it('should set X-XSS-Protection to 1; mode=block', (done) => {
+                        chai.request(server)
+                            .get(layer.route.path)
+                            .end((err, res) => {
+                                res.should.have.header('X-XSS-Protection', '1; mode=block');
+                                done();
+                            });
+                    });
+                });
+            }
         });
     });
 });
